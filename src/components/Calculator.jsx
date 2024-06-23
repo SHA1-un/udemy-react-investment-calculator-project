04346dcd7eb4eda6e7d929e5139f9aad195ee119
Shaun <shaun.oosthuizen.wp@gmail.com>
2024-06-23 12:19:50 +0200

    progress commit

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,10 @@
+export default function Calulator() {
+    return (
+        <section id="user-input">
+            <div id="input-group">
+                <input  type="text" />
+                <input  type="text" />
+            </div>
+        </section>
+    );
+}
